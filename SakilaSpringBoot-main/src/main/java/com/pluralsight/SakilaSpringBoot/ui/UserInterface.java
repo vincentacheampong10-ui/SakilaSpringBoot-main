9a2542ff5a6f2ca86f0801e944c58506139071fd
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-12-09 10:27:11 -0500

    have spring create amd run User Interface

--- SakilaSpringBoot-main/src/main/java/com/pluralsight/SakilaSpringBoot/ui/UserInterface.java
+++ SakilaSpringBoot-main/src/main/java/com/pluralsight/SakilaSpringBoot/ui/UserInterface.java
@@ -1,4 +1,14 @@
 package com.pluralsight.SakilaSpringBoot.ui;
 
-public class UserInterface {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserInterface implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Running User Interface");
+    }
 }
