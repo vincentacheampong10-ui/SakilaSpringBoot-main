7747730754fc637584c1b9d9127ffb1f57ae2907
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-12-09 11:33:58 -0500

    print actors

--- SakilaSpringBoot-main/src/main/java/com/pluralsight/SakilaSpringBoot/ui/UserInterface.java
+++ SakilaSpringBoot-main/src/main/java/com/pluralsight/SakilaSpringBoot/ui/UserInterface.java
@@ -1,14 +1,27 @@
 package com.pluralsight.SakilaSpringBoot.ui;
 
+import com.pluralsight.SakilaSpringBoot.data.ActorDao;
+import com.pluralsight.SakilaSpringBoot.model.Actor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class UserInterface implements CommandLineRunner {
+    @Autowired
+   private ActorDao actorDao;
+
 
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Running User Interface");
+        List<Actor> actors =actorDao.getAll();
+        for (Actor actor: actors){
+            System.out.println(actor);
+
+        }
     }
 }
